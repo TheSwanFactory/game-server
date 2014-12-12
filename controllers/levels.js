1240c479f06e5a50674bc3612f2e402ffb3bec91
Daniel Ma <drailskid@yahoo.com>
2014-12-11 22:24:45 -0800

    DELETE /levels/:id

--- controllers/levels.js
+++ controllers/levels.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var models = require('../models');
+var assert = require('assert');
 
 var controller = {
   // GET /levels
@@ -27,9 +28,22 @@ var controller = {
       .save()
       .then(function(level) {
         res.send(level);
-      }).catch(function(err) {
+      })
+      .catch(function(err) {
         res.status(422);
         res.send(err);
+      });
+  },
+
+  // DELETE /levels/:id
+  delete: function(req, res) {
+    assert.equal(req.headers['authorization'], process.env.API_KEY, 'wrong API key');
+    assert.ok(req.params.id, 'ID must be set');
+    models.Level
+      .find(req.params.id)
+      .destroy()
+      .then(function() {
+        res.send({success: true});
       });
   }
 };
--- routes/levels.js
+++ routes/levels.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var controller = require('../controllers/levels');
 
 /* GET users listing. */
-router.get('/',  controller.index);
-router.post('/', controller.create);
+router.get(   '/',    controller.index);
+router.post(  '/',    controller.create);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
